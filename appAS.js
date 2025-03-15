42981ab1e165ec9142d3f68130e1b6d5c92797e5
J4eden <gabrieltonietti1@gmail.com>
2025-03-15 16:58:30 -0300

    funcion agregar amigo
    - se implemento la función agregarAmigo, con una excepción para que no se genere una alerta antes del primer intento
    -se cambio el nombre del archivo .js por uno mas descriptivo y se aplico el cambio en el archivo .html

---
+++ appAS.js
@@ -0,0 +1,36 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let amigoNuevo=1;
+
+//function asignarTextoElemento(elemento, texto)
+        {
+            let elementoHTML = document.querySelector(elemento);
+            elementoHTML.innerHTML = texto;
+            return;
+        }
+
+
+function agregarAmigo()
+        {
+        //capturar valor
+        let amigoNuevo = document.getElementById('amigo').value
+        //validar entrada
+        if (amigoNuevo==''&& amigos.length>0)
+            {
+            //document.getElementById('amigo').setAttribute('placeholder', 'ingresa un nombre válido')
+            alert('Por favor, inserte un nombre')
+            }
+        //agregar al array
+        else if (amigoNuevo != '')
+        {
+            amigos.push(amigoNuevo)
+        }
+        //limpiar caja
+        let valorCaja = document.getElementById('amigo');
+            valorCaja.value = '';
+        }
+
+agregarAmigo();
+
+        //console.log(amigos);
